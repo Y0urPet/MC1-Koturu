7dba59db077e64ed81296e2b66beab2e76d61c34
timothy <tandrian24@bsd.idserve.net>
2024-04-01 21:34:14 +0700

    Initial Commit

---
+++ MC1-KoturuUITests/MC1_KoturuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MC1_KoturuUITestsLaunchTests.swift
+//  MC1-KoturuUITests
+//
+//  Created by Timothy Andrian on 01/04/24.
+//
+
+import XCTest
+
+final class MC1_KoturuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
